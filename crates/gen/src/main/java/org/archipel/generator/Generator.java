a39d7a41c68f05b530482a047e1ce4af305bdfdd
FlowArg <coutureflorence3@gmail.com>
2024-02-27 20:56:56 +0100

    refactor: fix algorithm & change stone block to lapis lazuli block for better visualization

--- crates/gen/src/main/java/org/archipel/generator/Generator.java
+++ crates/gen/src/main/java/org/archipel/generator/Generator.java
@@ -13,9 +13,9 @@ public class Generator
     public int getBlock(int x, int y, int z)
     {
         final var noise = this.perlinNoise.fractalBrownianMotion(x, z, 8);
-        final var surfaceLevel = y + noise * 20;
+        final var surfaceLevel = noise * 35;
         if(y < surfaceLevel)
-            return 1;
+            return 2;
         else return 0;
     }
 }
